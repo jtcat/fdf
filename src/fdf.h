163f6fbb961bad421cc9eae01bac43443f30d716
Joao Francisco <joaoteix@student.42porto.com>
2023-03-03 19:14:46 +0000

    add window boilerplate code

--- src/fdf.h
+++ src/fdf.h
@@ -6,16 +6,19 @@
 /*   By: joaoteix <joaoteix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 11:41:00 by joaoteix          #+#    #+#             */
-/*   Updated: 2023/03/03 17:51:42 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:45:54 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
-
-#include "libft.h"
-#include <fcntl.h>
-
 # define FDF_H
+
+# include "libft.h"
+# include <fcntl.h>
+
+# define WIN_HEIGHT 800
+# define WIN_WIDTH 600
+# define WIN_TITLE "FdF"
 
 typedef struct s_vec3
 {
